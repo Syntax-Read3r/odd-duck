42e70290051e5dd71db71b1827af5351cccb48fe
Munyaradzi Chikara <SyntaxRead3r@gmail.com>
2022-06-14 15:31:24 +0100

    add files to local storage

--- javascript/main.js
+++ javascript/main.js
@@ -1,173 +1,189 @@
 // TODO:
 // Create a constructorFn() that creates an objected associated with each product and has the following properties: Name of the product
-//						  File path of image of the product
-//  					  Number of times the image as been shown on the webapp
+// File path of image of the product
+// Number of times the image as been shown on the webapp
 // [x]
 function ProductImages(productName, productFilePath) {
-  this.name = productName;
-  this.path = productFilePath;
-  this.clickAcc = 0;
-  this.views = 0;
-  allProds.push(this);
+	this.name = productName;
+	this.path = productFilePath;
+	this.clickAcc = 0;
+	this.views = 0;
+	allProds.push(this);
 }
 
 let rounds = 25;
 let prevNumbers = [];
 
 ProductImages.prototype.render = function () {
-  this.views++;
-  const theProduct = this;
-  const imageEle = document.createElement("img");
-  imageEle.setAttribute("src", this.path);
-  imageEle.setAttribute("alt", this.name);
-  innerContainer.appendChild(imageEle);
+	this.views++;
+	const theProduct = this;
+	const imageEle = document.createElement("img");
+	imageEle.setAttribute("src", this.path);
+	imageEle.setAttribute("alt", this.name);
+	innerContainer.appendChild(imageEle);
 
-  imageEle.addEventListener("click", function () {
-    theProduct.clickAcc++;
+	imageEle.addEventListener("click", function () {
+		theProduct.clickAcc++;
 
-    if (rounds > 1) {
-      // if there are still rounds to go
-      rounds--;
-      getRandomImages();
-    } else {
-      //remove event listeners and display results
-      const button = document.getElementById("results-btn");
-      button.addEventListener("click", function () {
-        getResults();
-      });
-      button.removeAttribute("hidden");
-    }
-  });
+		if (rounds > 1) {
+			// if there are still rounds to go
+			rounds--;
+			getRandomImages();
+		} else {
+			//remove event listeners and display results
+			const button = document.getElementById("results-btn");
+			button.addEventListener("click", function () {
+				getResults();
+			});
+			button.removeAttribute("hidden");
+		}
+	});
 
-  if (rounds === 0) {
-    //remove event listener
-    imageEle.removeEventListener("click");
-  }
+	if (rounds === 0) {
+		//remove event listener
+		imageEle.removeEventListener("click");
+	}
 };
 
 var arr = [
-  "bag",
-  "banana",
-  "bathroom",
-  "boots",
-  "breakfast",
-  "bubblegum",
-  "chair",
-  "cthulhu",
-  "dog-duck",
-  "dragon",
-  "pen",
-  "pet-sweep",
-  "scissors",
-  "tauntaun",
-  "unicorn",
-  "water-can",
-  "wine-glass",
+	"bag",
+	"banana",
+	"bathroom",
+	"boots",
+	"breakfast",
+	"bubblegum",
+	"chair",
+	"cthulhu",
+	"dog-duck",
+	"dragon",
+	"pen",
+	"pet-sweep",
+	"scissors",
+	"tauntaun",
+	"unicorn",
+	"water-can",
+	"wine-glass",
 ];
-const allProds = [];
+let allProds = [];
 
 const innerContainer = document.getElementById("images");
 
-for (let x in arr) {
-  new ProductImages(arr[x], `./images/assets/${arr[x]}.jpg`);
+if (localStorage.getItem("allProds") === null) {
+  console.log('no local storage found')
+	for (let x in arr) {
+		new ProductImages(arr[x], `./images/assets/${arr[x]}.jpg`);
+	}
+} else {
+  console.log('local storage found');
+	let allProdsInStorage = JSON.parse(localStorage.getItem("allProds"));
+  for(let x in allProdsInStorage) {
+    console.log(allProdsInStorage[x])
+		let tempPro = new ProductImages(arr[x], `./images/assets/${arr[x]}.jpg`);
+    console.log(tempPro.clickAcc)
+    tempPro.clickAcc = allProdsInStorage[x].clickAcc;
+    tempPro.views = allProdsInStorage[x].views;
+  }
 }
+
+
 
 // TODO:
 // Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
 // [x]
 getRandomImages();
 function random() {
-  return (randomIndex = Math.floor(Math.random() * allProds.length));
+	return (randomIndex = Math.floor(Math.random() * allProds.length));
 }
 
 function getRandomImages() {
-  selected_Image = allProds[random()];
-  selected_Image2 = allProds[random()];
-  selected_Image3 = allProds[random()];
+	selected_Image = allProds[random()];
+	selected_Image2 = allProds[random()];
+	selected_Image3 = allProds[random()];
 
-  if (
-    selected_Image === selected_Image2 ||
-    selected_Image2 === selected_Image3 ||
-    selected_Image === selected_Image3
-  ) {
-    getRandomImages();
-  } else {
-    if (
-      prevNumbers.includes(allProds.indexOf(selected_Image)) ||
-      prevNumbers.includes(allProds.indexOf(selected_Image2)) ||
-      prevNumbers.includes(allProds.indexOf(selected_Image3))
-    ) {
-      console.log("Woops try again");
-      getRandomImages();
-    } else {
-      prevNumbers = [];
-      prevNumbers.push(allProds.indexOf(selected_Image));
-      prevNumbers.push(allProds.indexOf(selected_Image2));
-      prevNumbers.push(allProds.indexOf(selected_Image3));
-      innerContainer.innerHTML = "";
+	if (
+		selected_Image === selected_Image2 ||
+		selected_Image2 === selected_Image3 ||
+		selected_Image === selected_Image3
+	) {
+		getRandomImages();
+	} else {
+		if (
+			prevNumbers.includes(allProds.indexOf(selected_Image)) ||
+			prevNumbers.includes(allProds.indexOf(selected_Image2)) ||
+			prevNumbers.includes(allProds.indexOf(selected_Image3))
+		) {
+			console.log("Woops try again");
+			getRandomImages();
+		} else {
+			prevNumbers = [];
+			prevNumbers.push(allProds.indexOf(selected_Image));
+			prevNumbers.push(allProds.indexOf(selected_Image2));
+			prevNumbers.push(allProds.indexOf(selected_Image3));
+			innerContainer.innerHTML = "";
 
-      selected_Image.render();
-      selected_Image2.render();
-      selected_Image3.render();
-    }
-  }
+			selected_Image.render();
+			selected_Image2.render();
+			selected_Image3.render();
+		}
+	}
 }
 
 //Create a function that will display the results of the voting process.
 function getResults() {
-  const results = document.getElementById("results-list");
-  const data = [];
-  const labelNames = [];
+	localStorage.setItem("allProds", JSON.stringify(allProds));
+	const results = document.getElementById("results-list");
+	const data = [];
+	const labelNames = [];
 
-  for (let x in allProds) {
-    const itemClicked = allProds[x].clickAcc;
-    if (itemClicked > 0) {
-      data.push(itemClicked);
-      labelNames.push(allProds[x].name);
-      const listItem = document.createElement("li");
-      console.log(listItem);
-      listItem.innerHTML = `${allProds[x].name} had  ${allProds[x].clickAcc} votes and was viewed ${allProds[x].views} times`;
-      results.appendChild(listItem);
-    }
-  }
+	for (let x in allProds) {
+		const itemClicked = allProds[x].clickAcc;
+		if (itemClicked > 0) {
+			data.push(itemClicked);
+			labelNames.push(allProds[x].name);
+			const listItem = document.createElement("li");
+			console.log(listItem);
+			listItem.innerHTML = `${allProds[x].name} had  ${allProds[x].clickAcc} votes and was viewed ${allProds[x].views} times`;
+			results.appendChild(listItem);
+		}
+	}
 
-  const ctx = document.getElementById("chart").getContext("2d"); //get context is a chart js method stating a 2d graph.
+	const ctx = document.getElementById("chart").getContext("2d"); //get context is a chart js method stating a 2d graph.
 
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: labelNames,
-      datasets: [
-        {
-          label: "# of Votes",
-          data: data,
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-        },
-      ],
-    },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
-    },
-  });
+	new Chart(ctx, {
+		type: "bar",
+		data: {
+			labels: labelNames,
+			datasets: [
+				{
+					label: "# of Votes",
+					data: data,
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.2)",
+						"rgba(54, 162, 235, 0.2)",
+						"rgba(255, 206, 86, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(153, 102, 255, 0.2)",
+						"rgba(255, 159, 64, 0.2)",
+					],
+				},
+			],
+		},
+		options: {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+		},
+	});
 
-  results.removeAttribute("hidden");
-  const button = document.getElementById("results-btn");
-  button.setAttribute("hidden", true);
+	results.removeAttribute("hidden");
+	const button = document.getElementById("results-btn");
+	button.setAttribute("hidden", true);
 }
 
 //COMMENT: Click event and data.
